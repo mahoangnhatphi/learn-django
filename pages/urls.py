ca1a45ebe97c6d5a7f3e87551e2bcba9e3119d48
Ma Hoang Nhat Phi <mahoangnhatphi@gmail.com>
2021-10-06 03:09:17 +0700

    Add home page, about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from pages import views
+from pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', views.home_page)
+    path('', HomePageView.as_view(), name = 'home'),
+    path('about/', AboutPageView.as_view(), name = 'about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateView
 
 
-def home_page(req):
-    return HttpResponse('Hello World')
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
